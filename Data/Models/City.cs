8356714cc6a1bdcc5ef7fa2c78dcc58079acffd4
jshgalang <galang.joshaaron@gmail.com>
2021-06-20 23:14:14 +0800

    Initial commit

---
+++ Data/Models/City.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WorldCities.Data.Models
+{
+    [Table("Cities")]
+    public class City
+    {
+        #region Constructor
+        public City()
+        {
+        }
+        #endregion
+        #region Properties
+        /// <summary>
+        /// unique id and primary key for this city
+        ///</summary>
+        [Key]
+        [Required]
+        public int Id {get; set; }
+        /// <summary>
+        /// city name (in ascii)
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// city name
+        /// </summary>
+        public string Name_ASCII { get; set; }
+        /// <summary>
+        /// City Latitude
+        /// </summary>
+        public decimal Lat { get; set; }
+        ///<summary>
+        /// City Longitude
+        /// </summary>
+        public decimal Lon { get; set; }
+        ///<summary>
+        ///Country Id (foreign key)
+        ///</summary>
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        ///<summary>
+        ///country related to this city
+        ///</summary>
+        public virtual Country Country { get; set; }
+        #endregion
+    }
+}
---
+++ Data/Models/Country.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WorldCities.Data.Models
+{
+    [Table("Countries")]
+    public class Country
+    {
+        #region Constructor
+        public Country()
+        {
+        }
+        #endregion
+        #region Properties
+        /// <summary>
+        /// unique id
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// iso2 code
+        /// </summary>
+        public string ISO2 { get; set; }
+
+        /// <summary>
+        /// iso3 code
+        /// </summary>
+        public string ISO3 { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        /// <summary>
+        /// list containing of this country's cities
+        /// </summary>
+        public virtual List<City> Cities { get; set; }
+        #endregion
+    }
+}
